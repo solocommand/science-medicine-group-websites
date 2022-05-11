24611a12d168292899aac31e253a919901dc1dc3
Brandon Krigbaum <brandonbk@gmail.com>
2022-05-11 06:52:45 -0500

    Add start-server

---
+++ packages/global/start-server.js
@@ -0,0 +1,98 @@
+const newrelic = require('newrelic');
+const { startServer } = require('@parameter1/base-cms-marko-web');
+const { set, get, getAsObject } = require('@parameter1/base-cms-object-path');
+const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
+const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
+const omedaIdentityX = require('@parameter1/base-cms-marko-web-omeda-identity-x');
+const odentityCustomerUpsert = require('@parameter1/base-cms-marko-web-omeda/odentity/upsert-customer');
+
+const document = require('./components/document');
+const components = require('./components');
+const fragments = require('./fragments');
+const sharedRoutes = require('./routes');
+const paginated = require('./middleware/paginated');
+const redirectHandler = require('./redirect-handler');
+const oembedHandler = require('./oembed-handler');
+const omedaConfig = require('./config/omeda');
+const idxRouteTemplates = require('./templates/user');
+const recaptcha = require('./config/recaptcha');
+const idxNavItems = require('./config/identity-x-nav');
+
+const routes = (siteRoutes, siteConfig) => (app) => {
+  // Handle submissions on /__inquiry
+  loadInquiry(app);
+  // Shared/global routes (all sites)
+  sharedRoutes(app, siteConfig);
+  // Load site routes
+  siteRoutes(app);
+};
+
+module.exports = (options = {}) => {
+  const { onStart } = options;
+  const googleNewsInput = {
+    days: 7,
+    includeContentTypes: ['Article'],
+    excludeLabels: ['Sponsored', 'Sponsored by RoadPro'],
+  };
+  return startServer({
+    ...options,
+    routes: routes(options.routes, options.siteConfig),
+    document: options.document || document,
+    components: options.components || components,
+    fragments: options.fragments || fragments,
+    sitemapsHeaders: {
+      'x-google-news-input': JSON.stringify(googleNewsInput),
+    },
+    onStart: async (app) => {
+      if (typeof onStart === 'function') await onStart(app);
+      app.set('trust proxy', 'loopback, linklocal, uniquelocal');
+
+      // Use paginated middleware
+      app.use(paginated());
+
+      // Use paginated middleware
+      app.use(htmlSitemapPagination());
+
+      // Setup IdentityX + Omeda
+      const idxConfig = getAsObject(options, 'siteConfig.identityX');
+      omedaIdentityX(app, {
+        brandKey: omedaConfig.brandKey,
+        clientKey: omedaConfig.clientKey,
+        appId: omedaConfig.appId,
+        inputId: omedaConfig.inputId,
+        rapidIdentProductId: get(omedaConfig, 'rapidIdentification.productId'),
+        idxConfig,
+        idxRouteTemplates,
+      });
+      idxNavItems({ site: app.locals.site });
+
+      // Setup GAM.
+      const gamConfig = get(options, 'siteConfig.gam');
+      set(app.locals, 'GAM', gamConfig);
+
+      // Setup NativeX.
+      const nativeXConfig = get(options, 'siteConfig.nativeX');
+      set(app.locals, 'nativeX', nativeXConfig);
+
+      // i18n
+      const i18n = v => v;
+      set(app.locals, 'i18n', options.i18n || i18n);
+
+      // Recaptcha
+      set(app.locals, 'recaptcha', recaptcha);
+
+      // Omeda customer upsert
+      app.use(odentityCustomerUpsert({
+        brandKey: omedaConfig.brandKey,
+        onError: newrelic.noticeError.bind(newrelic),
+      }));
+    },
+    onAsyncBlockError: e => newrelic.noticeError(e),
+
+    redirectHandler,
+
+    embeddedMediaHandlers: {
+      oembed: oembedHandler,
+    },
+  });
+};
