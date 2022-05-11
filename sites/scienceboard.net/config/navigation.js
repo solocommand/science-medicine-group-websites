c32bd69d4c8bf11def9cece74c533b23aa27a596
Brandon Krigbaum <brandonbk@gmail.com>
2022-05-11 08:37:35 -0500

    Add navigation config

---
+++ sites/scienceboard.net/config/navigation.js
@@ -0,0 +1,75 @@
+const privacyPolicy = require('./privacy-policy');
+const subscribe = require('./subscribe');
+
+const topics = [
+  { href: '/bioprocessing', label: 'Bioprocessing' },
+  { href: '/cancer-research', label: 'Cancer Research' },
+  { href: '/cell-biology', label: 'Cell Biology' },
+  { href: '/drug-discovery', label: 'Drug Discovery' },
+  { href: '/gene-and-cell-therapy', label: 'Gene and Cell Therapy' },
+  { href: '/immunonology', label: 'Immunology' },
+  { href: '/omics', label: 'OMICs' },
+];
+
+const resources = [
+  { href: '/insights', label: 'Insights' },
+  { href: '/webinars', label: 'Webinars' },
+  { href: '/video', label: 'Video' },
+];
+
+const utilities = [
+  { href: '/page/advertise', label: 'Advertise' },
+  { href: '/page/contact-us', label: 'Contact Us' },
+];
+
+const mobileMenu = {
+  primary: [
+    ...topics,
+  ],
+  secondary: [
+    ...resources,
+    subscribe,
+    { href: '/', label: 'Advertise', target: '_blank' },
+  ],
+};
+
+const desktopMenu = {
+  about: [...utilities],
+  sections: [
+    ...topics,
+  ],
+};
+
+module.exports = {
+  type: 'navbar2',
+  promos: [
+    {
+      title: subscribe.label,
+      callToAction: subscribe.label,
+      link: subscribe.href,
+    },
+  ],
+  desktopMenu,
+  mobileMenu,
+  topics,
+  primary: {
+    items: topics,
+  },
+  secondary: {
+    items: [],
+  },
+  tertiary: {
+    items: [],
+  },
+  footer: {
+    items: [
+      privacyPolicy,
+      { href: '/page/contact-us', label: 'Contact Us' },
+      { href: '/site-map', label: 'Site Map' },
+    ],
+    topics: topics.primary,
+    more: [
+      ...utilities,
+    ],
+  },
+};
