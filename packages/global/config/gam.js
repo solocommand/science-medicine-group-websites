509483ecc2a51bb3d87bcb88d5e76847a46b3858
Brandon Krigbaum <brandonbk@gmail.com>
2022-05-12 09:00:33 -0500

    Adjust ad locations

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -16,33 +16,29 @@ module.exports = ({
         [265, 149], // Featured Pushdown / Fluid Banner
       ],
       sizeMapping: [
-        { viewport: [1070, 0], size: [[970, 90], [265, 149]] },
-        { viewport: [980, 0], size: [[970, 90], [265, 149], [736, 414]] },
-        { viewport: [750, 0], size: [[265, 149], [736, 414]] },
-        { viewport: [300, 0], size: [[320, 100], [300, 250], [320, 50], [265, 149], [428, 241]] },
+        { viewport: [980, 0], size: [[970, 250], [265, 149]] },
+        { viewport: [750, 0], size: [[265, 149]] },
+        { viewport: [300, 0], size: [[300, 250], [265, 149]] },
       ],
     })
     .setTemplate('ROTATION', {
-      size: [[970, 250], [970, 90], [728, 90], [320, 100]],
+      size: [[970, 250], [300, 250]],
       sizeMapping: [
-        { viewport: [980, 0], size: [[970, 250], [970, 90], [970, 66], [728, 90]] },
-        { viewport: [750, 0], size: [[728, 90], [300, 250]] },
-        { viewport: [300, 0], size: [[300, 50], [300, 250], [320, 100]] },
+        { viewport: [980, 0], size: [970, 250] },
+        { viewport: [750, 0], size: [300, 250] },
+        { viewport: [300, 0], size: [300, 250] },
       ],
     })
     .setTemplate('INLINE-CONTENT-MOBILE', {
-      size: [[970, 250], [970, 90], [728, 90], [320, 100]],
+      size: [300, 250],
       sizeMapping: [
-        { viewport: [980, 0], size: [] },
-        { viewport: [300, 0], size: [[300, 50], [300, 250], [320, 100]] },
+        { viewport: [300, 0], size: [300, 250] },
       ],
     })
     .setTemplate('INLINE-CONTENT-DESKTOP', {
-      size: [[970, 250], [970, 90], [728, 90], [320, 100]],
+      size: [300, 250],
       sizeMapping: [
-        { viewport: [980, 0], size: [[970, 250], [970, 90], [970, 66], [728, 90]] },
-        { viewport: [750, 0], size: [[728, 90], [300, 250]] },
-        { viewport: [0, 0], size: [] },
+        { viewport: [300, 0], size: [300, 250] },
       ],
     });
 
