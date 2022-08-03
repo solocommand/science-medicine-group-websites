1e674181328cb4270a8a632e667087d012f47d2d
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-03 16:07:00 -0500

    Add reveal-ad-handler

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,8 +2,10 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
+const RevealAdHandler = () => import(/* webpackChunkName: "reveal-ad-handler" */ './reveal-ad-handler.vue');
 
 export default (Browser) => {
   MonoRail(Browser);
   Browser.register('ContentMeterTrack', ContentMeterTrack);
+  Browser.register('RevealAdHandler', RevealAdHandler);
 };
