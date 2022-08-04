a91a3fdeebd9fec054f596e6ae2333cfd6063e2d
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-03 22:50:50 -0500

    Add LB-STICKY-BOTTOM template

--- packages/global/config/gam.js
+++ packages/global/config/gam.js
@@ -7,6 +7,19 @@ module.exports = ({
   const config = new GAMConfiguration(accountId, { basePath });
 
   config
+    .setTemplate('LB-STICKY-BOTTOM', {
+      size: [
+        [970, 90],
+        [728, 90],
+        [320, 50],
+        [300, 50],
+      ],
+      sizeMapping: [
+        { viewport: [980, 0], size: [[970, 90], [728, 90]] },
+        { viewport: [750, 0], size: [728, 90] },
+        { viewport: [320, 0], size: [[300, 50], [320, 50]] },
+      ],
+    })
     .setTemplate('LEADERBOARD', {
       size: [
         [970, 90],
