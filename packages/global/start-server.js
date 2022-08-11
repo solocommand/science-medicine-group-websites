60c61c53aee9ea7286c31becceddef5a337a7943
Brandon Krigbaum <brandonbk@gmail.com>
2022-08-11 15:47:34 -0500

    Remove omeda integration

--- packages/global/start-server.js
+++ packages/global/start-server.js
@@ -1,10 +1,8 @@
 const newrelic = require('newrelic');
 const { startServer } = require('@parameter1/base-cms-marko-web');
-const { set, get, getAsObject } = require('@parameter1/base-cms-object-path');
+const { set, get } = require('@parameter1/base-cms-object-path');
 const loadInquiry = require('@parameter1/base-cms-marko-web-inquiry');
 const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
-const omedaIdentityX = require('@parameter1/base-cms-marko-web-omeda-identity-x');
-const odentityCustomerUpsert = require('@parameter1/base-cms-marko-web-omeda/odentity/upsert-customer');
 
 const document = require('./components/document');
 const components = require('./components');
@@ -12,10 +10,7 @@ const fragments = require('./fragments');
 const sharedRoutes = require('./routes');
 const paginated = require('./middleware/paginated');
 const oembedHandler = require('./oembed-handler');
-const omedaConfig = require('./config/omeda');
-const idxRouteTemplates = require('./templates/user');
 const recaptcha = require('./config/recaptcha');
-const idxNavItems = require('./config/identity-x-nav');
 
 const routes = (siteRoutes, siteConfig) => (app) => {
   // Handle submissions on /__inquiry
@@ -52,19 +47,6 @@ module.exports = (options = {}) => {
       // Use paginated middleware
       app.use(htmlSitemapPagination());
 
-      // Setup IdentityX + Omeda
-      const idxConfig = getAsObject(options, 'siteConfig.identityX');
-      omedaIdentityX(app, {
-        brandKey: omedaConfig.brandKey,
-        clientKey: omedaConfig.clientKey,
-        appId: omedaConfig.appId,
-        inputId: omedaConfig.inputId,
-        rapidIdentProductId: get(omedaConfig, 'rapidIdentification.productId'),
-        idxConfig,
-        idxRouteTemplates,
-      });
-      idxNavItems({ site: app.locals.site });
-
       // Setup GAM.
       const gamConfig = get(options, 'siteConfig.gam');
       set(app.locals, 'GAM', gamConfig);
@@ -79,12 +61,6 @@ module.exports = (options = {}) => {
 
       // Recaptcha
       set(app.locals, 'recaptcha', recaptcha);
-
-      // Omeda customer upsert
-      app.use(odentityCustomerUpsert({
-        brandKey: omedaConfig.brandKey,
-        onError: newrelic.noticeError.bind(newrelic),
-      }));
     },
     onAsyncBlockError: e => newrelic.noticeError(e),
 
