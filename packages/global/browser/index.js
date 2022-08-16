e818ed509acee181c83c36847c74d15e808e7d27
Brian Miller <brian@parameter1.com>
2022-08-16 08:43:18 -0500

    Remove local revel/fixedpbottom-leader board refs
    Remove local copies of these components and update to use monorail package version.  Requires dep upgrad once latest version is released.

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,10 +2,8 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
-const RevealAdHandler = () => import(/* webpackChunkName: "reveal-ad-handler" */ './reveal-ad-handler.vue');
 
 export default (Browser) => {
-  MonoRail(Browser);
+  MonoRail(Browser, { enableOmedaIdentityX: false });
   Browser.register('ContentMeterTrack', ContentMeterTrack);
-  Browser.register('RevealAdHandler', RevealAdHandler);
 };
