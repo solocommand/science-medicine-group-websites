7a1a79303c46fbfc4effc955e185d1eda927e693
Brian Miller <brian@parameter1.com>
2022-08-22 07:23:50 -0500

    add category search component

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,8 +2,19 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
+const CompanySearch = () => import(/* webpackChunkName: "global-company-search" */ './company-search.vue');
+const SectionSearch = () => import(/* webpackChunkName: "global-section-search" */ './section-search.vue');
 
 export default (Browser) => {
+  const { EventBus } = Browser;
+
   MonoRail(Browser, { enableOmedaIdentityX: false });
+
+  Browser.register('GlobalCompanySearch', CompanySearch, {
+    provide: { EventBus },
+  });
+  Browser.register('GlobalSectionSearch', SectionSearch, {
+    provide: { EventBus },
+  });
   Browser.register('ContentMeterTrack', ContentMeterTrack);
 };
