55a70ab1f64fa0ed4c6ab8aa82bee9a157cf861a
Josh Worden <josh@parameter1.com>
2022-08-23 15:08:33 -0500

    Remove newsletter state management

--- sites/scienceboard.net/server/routes/content.js
+++ sites/scienceboard.net/server/routes/content.js
@@ -2,34 +2,33 @@ const withContent = require('@science-medicine-group/package-global/middleware/w
 const contentMeter = require('@science-medicine-group/package-global/middleware/content-meter');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/content-page');
 const contact = require('@science-medicine-group/package-global/templates/content/contact');
-const newsletterState = require('@science-medicine-group/package-global/middleware/newsletter-state');
 const company = require('../templates/content/company');
 const product = require('../templates/content/product');
 const whitepaper = require('../templates/content/whitepaper');
 const content = require('../templates/content');
 
 module.exports = (app) => {
-  app.get('/*?contact/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?contact/:id(\\d{8})*', withContent({
     template: contact,
     queryFragment,
   }));
 
-  app.get('/*?company/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?company/:id(\\d{8})*', withContent({
     template: company,
     queryFragment,
   }));
 
-  app.get('/*?product/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?product/:id(\\d{8})*', withContent({
     template: product,
     queryFragment,
   }));
 
-  app.get('/*?whitepaper/:id(\\d{8})*', newsletterState(), withContent({
+  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
     template: whitepaper,
     queryFragment,
   }));
 
-  app.get('/*?/:id(\\d{8})/*|/:id(\\d{8})(/|$)', newsletterState(), contentMeter(), withContent({
+  app.get('/*?/:id(\\d{8})/*|/:id(\\d{8})(/|$)', contentMeter(), withContent({
     template: content,
     queryFragment,
   }));
