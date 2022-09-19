15f659c31df3c5ac91f42cf7f5353b8e591a8e6d
Shinsina <shinsina@att.net>
2022-09-19 13:48:50 -0500

    Remove newsletter state (came from merge)

--- sites/labpulse.com/server/routes/home.js
+++ sites/labpulse.com/server/routes/home.js
@@ -1,6 +1,5 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
-const newsletterState = require('@science-medicine-group/package-global/middleware/newsletter-state');
 const home = require('../templates/index');
 
 module.exports = (app) => {
