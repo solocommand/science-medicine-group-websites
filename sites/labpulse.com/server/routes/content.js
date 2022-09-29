e28802fb004886751e731e148f56e5d13fee3efd
Brian Miller <brian@parameter1.com>
2022-09-29 15:49:21 -0500

    add newsletterState to content and section routes for lab

--- sites/labpulse.com/server/routes/content.js
+++ sites/labpulse.com/server/routes/content.js
@@ -1,4 +1,5 @@
 const withContent = require('@science-medicine-group/package-global/middleware/with-content');
+const { newsletterState, formatContentResponse } = require('@science-medicine-group/package-global/middleware/newsletter-state');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/content-page');
 const contact = require('@science-medicine-group/package-global/templates/content/contact');
 const company = require('../templates/content/company');
@@ -7,28 +8,33 @@ const whitepaper = require('../templates/content/whitepaper');
 const content = require('../templates/content');
 
 module.exports = (app) => {
-  app.get('/*?contact/:id(\\d{8})*', withContent({
+  app.get('/*?contact/:id(\\d{8})*', newsletterState({ setCookie: false }), newsletterState({ setCookie: false }), withContent({
     template: contact,
     queryFragment,
+    formatResponse: formatContentResponse,
   }));
 
-  app.get('/*?company/:id(\\d{8})*', withContent({
+  app.get('/*?company/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: company,
     queryFragment,
+    formatResponse: formatContentResponse,
   }));
 
-  app.get('/*?product/:id(\\d{8})*', withContent({
+  app.get('/*?product/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: product,
     queryFragment,
+    formatResponse: formatContentResponse,
   }));
 
-  app.get('/*?whitepaper/:id(\\d{8})*', withContent({
+  app.get('/*?whitepaper/:id(\\d{8})*', newsletterState({ setCookie: false }), withContent({
     template: whitepaper,
     queryFragment,
+    formatResponse: formatContentResponse,
   }));
 
-  app.get('/*?/:id(\\d{8})/*|/:id(\\d{8})(/|$)', withContent({
+  app.get('/*?/:id(\\d{8})/*|/:id(\\d{8})(/|$)', newsletterState({ setCookie: false }), withContent({
     template: content,
     queryFragment,
+    formatResponse: formatContentResponse,
   }));
 };
