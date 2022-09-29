18aea9d99697698aef41a43456716d94cc69043f
Brian Miller <brian@parameter1.com>
2022-09-28 20:04:46 -0500

    update lab newsletter config

--- sites/labpulse.com/config/newsletter.js
+++ sites/labpulse.com/config/newsletter.js
@@ -1,19 +1,22 @@
-const baseConfig = {
+const defaults = {
+  name: 'Don’t Miss Out',
+  description: 'Breaking, business, and industry news about the clinical lab community.',
   action: '/user/subscribe',
   hiddenInputs: [],
 };
 
 module.exports = {
+  pushdown: {
+    ...defaults,
+    // imagePath: 'files/base/randallreilly/all/image/static/newsletter-pushdown/ovd-half.png',
+  },
   signupBannerLarge: {
-    ...baseConfig,
-    name: 'Don’t Miss Out',
-    description: 'Breaking, business, and industry news about the clinical lab community.',
+    ...defaults,
   },
   signupFooter: {
-    ...baseConfig,
+    ...defaults,
     colspan: 5,
     enable: true,
     name: 'Stay Connected',
-    description: 'Breaking, business, and industry news about the clinical lab community.',
   },
 };
