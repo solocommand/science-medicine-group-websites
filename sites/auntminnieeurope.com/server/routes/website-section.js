a7884d508f1eb319569c5c1db11c603d70856440
Brandon Krigbaum <brandonbk@gmail.com>
2022-10-14 08:29:00 -0500

    Update routes

--- sites/auntminnieeurope.com/server/routes/website-section.js
+++ sites/auntminnieeurope.com/server/routes/website-section.js
@@ -1,15 +1,14 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
-const newsletterState = require('@science-medicine-group/package-global/middleware/newsletter-state');
 const upcoming = require('@science-medicine-group/package-global/templates/website-section/upcoming');
 const section = require('../templates/website-section');
 
 module.exports = (app) => {
-  app.get('/:alias(resources/conferences)', newsletterState(), withWebsiteSection({
+  app.get('/:alias(resources/conferences)', withWebsiteSection({
     template: upcoming,
     queryFragment,
   }));
-  app.get('/:alias([a-z0-9-/]+)', newsletterState(), withWebsiteSection({
+  app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
   }));
--- sites/scienceboard.net/server/routes/website-section.js
+++ sites/scienceboard.net/server/routes/website-section.js
@@ -1,8 +1,13 @@
 const { withWebsiteSection } = require('@parameter1/base-cms-marko-web/middleware');
 const queryFragment = require('@parameter1/base-cms-marko-web-theme-monorail/graphql/fragments/website-section-page');
+const upcoming = require('@science-medicine-group/package-global/templates/website-section/upcoming');
 const section = require('../templates/website-section');
 
 module.exports = (app) => {
+  app.get('/:alias(resources/conferences)', withWebsiteSection({
+    template: upcoming,
+    queryFragment,
+  }));
   app.get('/:alias([a-z0-9-/]+)', withWebsiteSection({
     template: section,
     queryFragment,
