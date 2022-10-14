1b6db253c8e039085abdc34ba4d1821f18c8ee89
Brandon Krigbaum <brandonbk@gmail.com>
2022-10-14 07:47:47 -0500

    Add am site

---
+++ sites/auntminnie.com/config/site.js
@@ -0,0 +1,66 @@
+const navigation = require('./navigation');
+const contentMeter = require('./content-meter');
+const gam = require('./gam');
+const nativeX = require('./native-x');
+const identityX = require('./identity-x');
+const newsletter = require('./newsletter');
+const search = require('./search');
+
+module.exports = {
+  navigation,
+  contentMeter,
+  gam,
+  nativeX,
+  identityX,
+  idxNavItems: {
+    enable: process.env.IDX_NAV_ENABLE || false,
+  },
+  newsletter,
+  search,
+  company: 'Science and Medicine Group',
+  p1events: {
+    tenant: 'smg',
+    enabled: true,
+    cookieDomain: process.env.NODE_ENV === 'production' ? 'auntminnie.com' : '',
+  },
+  logos: {
+    navbar: {
+      src: 'https://img.auntminnie.com/files/base/smg/all/image/static/am/am-logo.svg?h=45&auto=format,compress',
+      srcset: [
+        'https://img.auntminnie.com/files/base/smg/all/image/static/am/am-logo.svg?h=90&auto=format,compress 2x',
+      ],
+    },
+    footer: {
+      src: 'https://img.auntminnie.com/files/base/smg/all/image/static/am/am-logo.svg?h=60&auto=format,compress',
+      srcset: [
+        'https://img.auntminnie.com/files/base/smg/all/image/static/am/am-logo.svg?h=120&auto=format,compress 2x',
+      ],
+    },
+    corporate: {
+      alt: 'Science and Medicine Group Logo',
+      href: 'https://scienceandmedicinegroup.com',
+      src: 'https://img.auntminnie.com/files/base/smg/all/image/static/smg-logo.png?w=200&auto=format,compress',
+      srcset: [
+        'https://img.auntminnie.com/files/base/smg/all/image/static/smg-logo.png?w=200&auto=format,compress&dpr=2 2x',
+      ],
+    },
+  },
+  socialMediaLinks: [
+    { provider: 'facebook', href: 'https://www.facebook.com/auntminnie', target: '_blank' },
+    { provider: 'twitter', href: 'https://twitter.com/auntminnie', target: '_blank' },
+    { provider: 'linkedin', href: 'https://www.linkedin.com/company/auntminnie.com/', target: '_blank' },
+  ],
+  podcastLinks: [],
+  gtm: {
+    containerId: 'GTM-5RJ8D63',
+  },
+  inquiry: {
+    enabled: false,
+    directSend: false,
+    sendTo: 'support@parameter1.com',
+    sendFrom: 'auntminnie.com <noreply@parameter1.com>',
+    logo: 'https://img.auntminnie.com/files/base/smg/all/image/static/am/am-logo.svg?h=45&auto=format,compress&bg=000000&pad=5',
+    bgColor: '#000000',
+  },
+  setSearchSortFieldToScore: true,
+};
