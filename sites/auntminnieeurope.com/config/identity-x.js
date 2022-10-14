90fbd5c0123ca7ef932cebef735a04eb8c8a5a37
Brandon Krigbaum <brandonbk@gmail.com>
2022-10-14 07:48:06 -0500

    Add ame site

---
+++ sites/auntminnieeurope.com/config/identity-x.js
@@ -0,0 +1,3 @@
+const configureIdentityX = require('@science-medicine-group/package-global/config/identity-x');
+
+module.exports = configureIdentityX({ appId: '62faabea1f57339870579cd6' });
---
+++ sites/auntminnieeurope.com/config/search.js
@@ -0,0 +1,5 @@
+module.exports = {
+  assignedToWebsiteSectionIds: [
+    11111,
+  ],
+};
