ef949034eb2feea78f29097947099ac333307de4
Josh Worden <josh@parameter1.com>
2022-10-27 15:42:24 -0500

    Convert to Vue component, use IdX user context

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -2,6 +2,7 @@
 import MonoRail from '@parameter1/base-cms-marko-web-theme-monorail/browser';
 import Braze from '@science-medicine-group/package-braze/browser';
 import FormLogin from './form-login.vue';
+import Rudderstack from './rudderstack.vue';
 
 const GlobalSiteNewsletterMenu = () => import(/* webpackChunkName: "global-site-newsletter-menu" */ './site-newsletter-menu.vue');
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
@@ -21,12 +22,5 @@ export default (Browser) => {
   Braze(Browser);
 
   // Rudderstack identification
-  EventBus.$on('identity-x-login-link-sent', ({ email }) => {
-    const fn = window.rudderanalytics ? window.rudderanalytics.identify : () => {};
-    fn({ email });
-  });
-  EventBus.$on('identity-x-authenticated', ({ id, email }) => {
-    const fn = window.rudderanalytics ? window.rudderanalytics.identify : () => {};
-    fn(id, { email });
-  });
+  Browser.register('Rudderstack', Rudderstack, { provide: { EventBus } });
 };
