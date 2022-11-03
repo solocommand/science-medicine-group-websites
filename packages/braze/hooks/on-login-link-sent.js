1ffaef70e89a38df7cb39e62f579af25a828b367
Josh Worden <josh@parameter1.com>
2022-11-03 13:00:46 -0500

    Change login link logic to opt-in to unconfirmed

--- packages/braze/hooks/on-login-link-sent.js
+++ packages/braze/hooks/on-login-link-sent.js
@@ -1,5 +1,3 @@
-const { updateIdentityXUser } = require('../utils');
-
 /**
  *
  */
@@ -10,17 +8,14 @@ module.exports = async ({
 }) => {
   // Only set opt-ins from newsletter signup controls, not on every login!
   if (actionSource !== 'newsletterSignup') return;
+  // Only opt-in to unconfirmed if the user is not yet verified.
+  if (user.verified) return;
 
   const { req } = service;
   const { braze } = req;
 
   // Mark all subscription group questions as `true`.
-  const questions = await braze.getSubscriptionGroupQuestions(service);
-  const optIns = questions.reduce((obj, q) => ({ ...obj, [q.groupId]: true }), {});
-  const answers = questions.map(q => ({ fieldId: q.id, value: true }));
-
-  // Update the IdentityX user profile
-  await updateIdentityXUser(user.email, service, answers);
+  const optIns = { [braze.getUnconfirmedGroupId()]: true };
 
   // Update the Braze subscription data
   await braze.updateSubscriptions(user.email, user.id, optIns);
