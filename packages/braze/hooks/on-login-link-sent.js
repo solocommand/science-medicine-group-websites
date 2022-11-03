d30719fb85738d0420969d25b9f7845be160b4d7
Josh Worden <josh@parameter1.com>
2022-11-03 16:01:31 -0500

    Unify service logic

--- packages/braze/hooks/on-login-link-sent.js
+++ packages/braze/hooks/on-login-link-sent.js
@@ -6,17 +6,15 @@ module.exports = async ({
   service,
   actionSource,
 }) => {
-  // Only set opt-ins from newsletter signup controls, not on every login!
-  if (actionSource !== 'newsletterSignup') return;
-  // Only opt-in to unconfirmed if the user is not yet verified.
-  if (user.verified) return;
-
   const { req } = service;
   const { braze } = req;
 
-  // Mark all subscription group questions as `true`.
-  const optIns = { [braze.getUnconfirmedGroupId()]: true };
+  // Only set opt-ins from newsletter signup controls, not on every login!
+  if (actionSource !== 'newsletterSignup') return;
 
-  // Update the Braze subscription data
-  await braze.updateSubscriptions(user.email, user.id, optIns);
+  // Only opt-in to unconfirmed if the user is not yet verified.
+  if (user.verified) return;
+
+  // Opt the user into the 'unconfirmed' group
+  await braze.unconfirmUser(user.email, user.id);
 };
