e4582004c6e4518090575477c9ef6973d631f9f2
Josh Worden <josh@parameter1.com>
2022-11-03 14:05:52 -0500

    Set temporary values for unconfirmed group ids

--- sites/drbicuspid.com/config/braze.js
+++ sites/drbicuspid.com/config/braze.js
@@ -1,5 +1,9 @@
 const config = require('@science-medicine-group/package-global/config/braze');
 
+const { log } = console;
+log('Create/set unconfirmedGroupId!');
+
 module.exports = config({
   siteName: 'DrBicuspid',
+  unconfirmedGroupId: '~not-set~',
 });
