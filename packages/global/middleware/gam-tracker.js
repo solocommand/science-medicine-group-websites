1585b161c618ac10cd8dbdd1ef08afd53781ffb7
Josh Worden <josh@parameter1.com>
2022-11-22 14:45:48 -0600

    Move gam tracker to single file

---
+++ packages/global/middleware/gam-tracker.js
@@ -0,0 +1,40 @@
+const { set } = require('@parameter1/base-cms-object-path');
+const { asyncRoute } = require('@parameter1/base-cms-utils');
+const fetch = require('node-fetch');
+
+module.exports = asyncRoute(async (req, res, next) => {
+  try {
+    const { GAM_TRACK_API_KEY } = process.env;
+    if (!GAM_TRACK_API_KEY) return next();
+    const { identityX } = res.locals;
+    if (!identityX) return next();
+    const context = await identityX.loadActiveContext();
+    const { application, user } = context;
+    if (!application || !user) return next();
+
+    const r = await fetch('https://api.gt.parameter1.dev', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json', 'x-api-key': GAM_TRACK_API_KEY },
+      body: JSON.stringify({
+        action: 'encrypt',
+        params: {
+          identities: [{
+            provider: 'identity-x',
+            tenant: application.id,
+            entityType: 'app-user',
+            id: user.id,
+          }],
+        },
+      }),
+    });
+    if (!r.ok) throw new Error('Bad fetch response');
+    const { data } = await r.json();
+    set(res.locals, 'gamTrackTargeting', data);
+    return next();
+  } catch (e) {
+    // @todo log this, don't break the request.
+    const { error } = console;
+    error('GAM TRACKER ERROR!', e);
+    return next();
+  }
+});
