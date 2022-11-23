587bffe57b84b686a880d9b3f2bbe797d99a67a7
Josh Worden <josh@parameter1.com>
2022-11-23 11:04:14 -0600

    Allow ClientID and issuerBaseURL to be env’d

--- sites/drbicuspid.com/config/auth0.js
+++ sites/drbicuspid.com/config/auth0.js
@@ -1,6 +1,6 @@
 module.exports = {
   baseURL: process.env.AUTH0_BASEURL,
-  clientID: '8aHKTbVEAMFdtF3S2sA67zoprlfT2TVw',
+  clientID: process.env.AUTH0_CLIENTID || '8aHKTbVEAMFdtF3S2sA67zoprlfT2TVw',
   clientSecret: process.env.AUTH0_SECRET,
-  issuerBaseURL: 'https://dev-scienceandmedicinegroup.us.auth0.com',
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASEURL || 'https://dev-scienceandmedicinegroup.us.auth0.com',
 };
