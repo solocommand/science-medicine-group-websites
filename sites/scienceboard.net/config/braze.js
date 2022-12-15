4e31b06bc37bf683327067514db2d001c52c8ab8
Josh Worden <josh@parameter1.com>
2022-12-15 15:56:38 -0600

    Remove unconfirmedGroup logic/configs

--- sites/scienceboard.net/config/braze.js
+++ sites/scienceboard.net/config/braze.js
@@ -1,10 +1,6 @@
 const config = require('@science-medicine-group/package-global/config/braze');
 
-const { log } = console;
-log('Create/set unconfirmedGroupId!');
-
 module.exports = config({
   siteName: 'SAB',
-  unconfirmedGroupId: '~not-set~',
   appGroupId: '634d62e10ef4e37a75846f41',
 });
