da87033ca84340796a4d6c45499f1b99f237e6f7
Josh Worden <josh@parameter1.com>
2022-12-15 15:58:47 -0600

    Add user context links to nav

--- sites/drbicuspid.com/config/navigation.js
+++ sites/drbicuspid.com/config/navigation.js
@@ -36,7 +36,40 @@ module.exports = {
     },
   ],
   user: {
-    items: [],
+    items: [
+      {
+        href: '/login',
+        label: 'Sign In',
+        when: 'logged-out',
+        modifiers: ['user'],
+      },
+      {
+        href: '/logout',
+        label: 'Sign Out',
+        when: 'logged-in',
+        modifiers: ['user'],
+      },
+    ],
+    tools: [
+      {
+        href: '/login',
+        label: 'Sign In',
+        when: 'logged-out',
+        modifiers: ['user'],
+      },
+      {
+        href: '/user/profile',
+        label: 'Modify profile',
+        when: 'logged-in',
+        modifiers: ['user'],
+      },
+      {
+        href: '/logout',
+        label: 'Sign Out',
+        when: 'logged-in',
+        modifiers: ['user'],
+      },
+    ],
   },
   mobileMenu,
   topics,
