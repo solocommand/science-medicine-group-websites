996dd0be4bf32ceff1bab25669bbc4914f7415ff
Josh Worden <josh@parameter1.com>
2022-12-15 15:59:05 -0600

    Update DrB Auth0 config

--- sites/drbicuspid.com/config/auth0.js
+++ sites/drbicuspid.com/config/auth0.js
@@ -1,6 +1,8 @@
 module.exports = {
   baseURL: process.env.AUTH0_BASEURL,
-  clientID: process.env.AUTH0_CLIENTID || '8aHKTbVEAMFdtF3S2sA67zoprlfT2TVw',
+  clientID: process.env.AUTH0_CLIENTID || '6GhwIT6bvSyzlkjn8h7QhDb6EavxasKo',
   clientSecret: process.env.AUTH0_SECRET,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASEURL || 'https://dev-scienceandmedicinegroup.us.auth0.com',
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASEURL || 'https://dev.login.drbicuspid.com',
+  // Set Audience URL for Management API due to custom issuer domain
+  apiAudienceURL: process.env.AUTH0_API_AUDIENCE_URL || 'https://dev-scienceandmedicinegroup.us.auth0.com',
 };
