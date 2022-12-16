a05dbce094be2cc74e62380d76fdc025bce7dbab
Brian Miller <brian@parameter1.com>
2022-12-16 12:18:19 -0600

    add content meter config to lab

---
+++ sites/labpulse.com/config/content-meter.js
@@ -0,0 +1,7 @@
+const defaultConfig = require('@science-medicine-group/package-global/config/content-meter');
+
+module.exports = {
+  ...defaultConfig,
+  promoCode: 'lab_registration_meter',
+  enable: true,
+};
