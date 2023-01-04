94dea39d32ee248d447678b4ec29133ab5d628bd
Jessica Wade <jessica.wade@parameter1.com>
2023-01-04 14:52:16 -0600

    Add billboard and reskin ads to labpulse

--- sites/labpulse.com/config/gam.js
+++ sites/labpulse.com/config/gam.js
@@ -11,12 +11,13 @@ config.lazyLoad = {
 
 config.setAliasAdUnits('default', [
   { name: 'lb-sticky-bottom', templateName: 'LB-STICKY-BOTTOM', path: 'leaderboard' },
+  { name: 'billboard', templateName: 'BILLBOARD', path: 'billboard' },
   { name: 'leaderboard', templateName: 'LEADERBOARD', path: 'leaderboard' },
   { name: 'rotation', templateName: 'ROTATION', path: 'rotation' },
   { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: 'rotation' },
   { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: 'rotation' },
   // Below based on enableRevealAd
-  // { name: 'reskin', path: 'reskin' },
+  { name: 'reskin', path: 'reskin' },
 ]);
 
 const aliases = [
@@ -29,6 +30,7 @@ const aliases = [
 
 aliases.forEach(alias => config.setAliasAdUnits(alias, [
   { name: 'leaderboard', templateName: 'LEADERBOARD', path: `${alias}-leaderboard` },
+  { name: 'billboard', templateName: 'BILLBOARD', path: `${alias}-billboard` },
   { name: 'rotation', templateName: 'ROTATION', path: `${alias}-rotation` },
   { name: 'inline-content-mobile', templateName: 'INLINE-CONTENT-MOBILE', path: `${alias}-rotation` },
   { name: 'inline-content-desktop', templateName: 'INLINE-CONTENT-DESKTOP', path: `${alias}-rotation` },
