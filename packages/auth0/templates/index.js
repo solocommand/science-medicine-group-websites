4b1ea277389ccb8e6fd120210d912a427a3755c2
Josh Worden <josh@parameter1.com>
2023-01-10 12:49:28 -0600

    Add invalid email check

--- packages/auth0/templates/index.js
+++ packages/auth0/templates/index.js
@@ -1,7 +1,9 @@
+const dbEmailInvalid = require('./db-email-invalid');
 const dbEmailVerification = require('./db-email-verification');
 const noEmail = require('./no-email');
 
 module.exports = {
   dbEmailVerification,
+  dbEmailInvalid,
   noEmail,
 };
