a477b443bb4dd891f999adcbca725c3a1ba4bedc
Brian Miller <brian@parameter1.com>
2023-01-10 08:07:55 -0600

    Also enable reskin on staging

--- sites/labpulse.com/config/gam.js
+++ sites/labpulse.com/config/gam.js
@@ -2,6 +2,8 @@ const configureGAM = require('@science-medicine-group/package-global/config/gam'
 
 const config = configureGAM({ basePath: 'lab' });
 
+config.enableRevealAd = true;
+
 config.lazyLoad = {
   enabled: true, // set to true to enable lazy loading
   fetchMarginPercent: 100, // fetch ad when one viewport away
