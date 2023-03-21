a57fb8dca546cbb900e706e3f124cbc945d523c1
Josh Worden <josh@parameter1.com>
2023-03-21 15:50:47 -0500

    Restore unconfirmedGroupId configs

--- sites/drbicuspid.com/config/braze.js
+++ sites/drbicuspid.com/config/braze.js
@@ -2,5 +2,6 @@ const config = require('@science-medicine-group/package-global/config/braze');
 
 module.exports = config({
   siteName: 'DrBicuspid',
+  unconfirmedGroupId: '3a0e5e67-8fd8-41fd-979e-c4f834815e4c',
   appGroupId: '634d67c3ee91fe5375c3bcac',
 });
