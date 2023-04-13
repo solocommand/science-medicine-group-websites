c365aa0670281e042cb4a474e402ff9eec556446
Josh Worden <josh@parameter1.com>
2023-04-13 13:14:27 -0500

    swap endpoint/hostname configs

--- sites/auntminnie.com/config/wp-icle.js
+++ sites/auntminnie.com/config/wp-icle.js
@@ -1,8 +1,8 @@
 module.exports = {
   // If the entire sync process is enabled. Driven by env to bypass in integration tests
   enabled: ['1', 'true'].includes(process.env.WPICLE_ENABLED),
-  // The WP-ICLE API endpoint to use (varies by tenant/site and environment)
-  endpoint: process.env.WPICLE_API_ENDPOINT,
+  // The WP-ICLE API hostname to use (varies by tenant/site and environment)
+  hostname: process.env.WPICLE_API_HOSTNAME || 'my.auntminnie.com',
   // The WP-ICLE API key to use (varies by endpoint)
   apiKey: process.env.WPICLE_API_KEY,
   // The SQS queue to use
@@ -10,5 +10,4 @@ module.exports = {
   // AWS credentials to push to queue
   accessKey: process.env.WPICLE_AWS_ACCESS_KEY_ID,
   secretKey: process.env.WPICLE_AWS_SECRET_ACCESS_KEY,
-  hostname: process.env.WPICLE_HOSTNAME || 'my.auntminnie.com',
 };
