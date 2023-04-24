37cc47bfecad16f39ef2a4d5d8c282b568486709
Josh Worden <josh@parameter1.com>
2023-04-24 15:21:01 -0500

    Fix content gating middleware

--- packages/global/middleware/content-gating.js
+++ packages/global/middleware/content-gating.js
@@ -1,29 +1,28 @@
-const { get, getAsArray } = require('@parameter1/base-cms-object-path');
+const { get } = require('@parameter1/base-cms-object-path');
 const contentGating = require('@parameter1/base-cms-marko-web-theme-monorail/middleware/content-gating');
 const getCookie = require('../utils/get-cookie');
 
 const COOKIE_NAME = '__idx_gating';
 
 /**
- * Returns a tuple of the enabled/disabled state and required access level ids
+ * Returns the enabled/disabled state of the content object
  *
  * @param content Object
  * @param req Object
  * @param res Object
  *
- * @returns Promise([Boolean, String[]])
+ * @returns Promise(Boolean)
  */
 module.exports = (app) => app.use((req, res, next) => {
-  contentGating(app, true, ({ content }) => {
+  const fn = ({ content }) => {
     const { identityX } = req;
     const requiresRegistration = get(content, 'userRegistration.isCurrentlyRequired');
-    const accessLevels = getAsArray(content, 'userRegistration.accessLevels');
 
     // If the content isn't gated, do nothing.
-    if (!requiresRegistration) return [requiresRegistration, accessLevels];
+    if (!requiresRegistration) return false;
 
     // If a user is logged in, enable and don't do anything special.
-    if (identityX && identityX.token) return [requiresRegistration, accessLevels];
+    if (identityX && identityX.token) return requiresRegistration;
 
     // Check for `__idx_gating` cookie, set by Braze Identity middleware
     const cookie = getCookie({ req, res, name: COOKIE_NAME });
@@ -31,14 +30,16 @@ module.exports = (app) => app.use((req, res, next) => {
     if (cookie) {
       try {
         const allowed = JSON.parse(cookie);
-        return [!allowed, accessLevels];
+        return !allowed;
       } catch (e) {
         // noop
       }
     }
 
     // Final fallback
-    return [requiresRegistration, accessLevels];
-  });
+    return requiresRegistration;
+  };
+
+  contentGating(app, true, fn);
   next();
 });
