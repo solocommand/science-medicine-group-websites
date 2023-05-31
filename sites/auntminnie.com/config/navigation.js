d9ac5a16d03ae288a2554b774df8eccc56cea3b2
Jessica Wade <jessica.wade@parameter1.com>
2023-05-31 09:53:42 -0500

    Update Navigation

--- sites/auntminnie.com/config/navigation.js
+++ sites/auntminnie.com/config/navigation.js
@@ -1,27 +1,82 @@
-const sortNavItems = require('@science-medicine-group/package-global/utils/sort-nav-items');
 const icleCfg = require('./wp-icle');
-
 const subscribe = require('./subscribe');
 const privacyPolicy = require('./privacy-policy');
 
+const clinicalNews = [
+  { href: '/clinical-news/ct', label: 'CT' },
+  { href: '/clinical-news/digital-x-ray', label: 'Digital X-Ray' },
+  { href: '/clinical-news/interventional', label: 'Interventional' },
+  { href: '/clinical-news/molecular-imaging', label: 'Molecular Imaging' },
+  { href: '/clinical-news/mri', label: 'MRI' },
+  { href: '/clinical-news/radiation-oncology-therapy', label: 'Radiation Oncology/Therapy' },
+  { href: '/clinical-news/ultrasound', label: 'Ultrasound' },
+  { href: '/clinical-news/womens-imaging', label: 'Womens Imaging' },
+];
+const imagingInformatics = [
+  { href: '/imaging-informatics/advanced-visualization', label: 'Advanced Visualization' },
+  { href: '/imaging-informatics/enterprise-imaging', label: 'Enterprise Imaging' },
+  { href: '/imaging-informatics/artifical-intelligence', label: 'Artifical Intelligence' },
+  { href: '/imaging-informatics/cybersecurity', label: 'Cybersecurity' },
+];
+const industryNews = [
+  { href: '/industry-news/installations', label: 'Installations' },
+  { href: '/industry-news/product-news', label: 'Product News' },
+  { href: '/industry-news/regulatory', label: 'Regulatory' },
+  { href: '/industry-news/ma', label: 'M&A' },
+  { href: '/industry-news/market-analysis', label: 'Market Analysis' },
+];
+const practiceManagement = [
+  { href: '/practice-management/administration', label: 'Administration' },
+  { href: '/practice-management/associations', label: 'Associations' },
+  { href: '/practice-management/careers', label: 'Careers' },
+  { href: '/practice-management/equity-inclusion', label: 'Equity & Inclusion' },
+  { href: '/practice-management/medicolegal', label: 'Medicolegal' },
+  { href: '/practice-management/patient-safety', label: 'Patient Safety' },
+  { href: '/practice-management/radiologic-technologist', label: 'Radiologic Technologist' },
+  { href: '/practice-management/service', label: 'Service' },
+];
+const radiologyEducation = [
+  { href: '/radiology-education/medical-students', label: 'Medical Students' },
+  { href: '/radiology-education/residents-fellows', label: 'Residents/Fellows' },
+];
+const subspecialties = [
+  { href: '/subspecialties/breast-imaging', label: 'Breast Imagaing' },
+  { href: '/subspecialties/cardiovascular-radiology', label: 'Cardiovascular Radiology' },
+  { href: '/subspecialties/chest-radiology', label: 'Chest Radiology' },
+  { href: '/subspecialties/emergency-radiology', label: 'Emergency Radiology' },
+  { href: '/subspecialties/gastrointestinal-radiology', label: 'Gastrointestinal Radiology' },
+  { href: '/subspecialties/genitourinary-radiology', label: 'Genitourinary Radiology' },
+  { href: '/subspecialties/head-and-neck-radiology', label: 'Head and Neck Radiology' },
+  { href: '/subspecialties/interventional-radiology', label: 'Interventional Radiology' },
+  { href: '/subspecialties/medical-physics', label: 'Medical Physics' },
+  { href: '/subspecialties/musculoskeletal-radiology', label: 'Musculoskeletal Radiology' },
+  { href: '/subspecialties/neuroradiology', label: 'Neuroradiology' },
+  { href: '/subspecialties/nuclear-radiology', label: 'Nuclear Radiology' },
+  { href: '/subspecialties/pediatric-radiology', label: 'Pediatric Radiology' },
+  { href: '/subspecialties/radiation-oncology', label: 'Radiation Oncology' },
+];
+
 const topics = [
-  { href: '/clinical-news', label: 'Clinical News' },
-  { href: '/imaging-informatics', label: 'Imaging Informatics' },
-  { href: '/industry-news', label: 'Industry News' },
-  { href: '/practice-management', label: 'Practice Management' },
-  { href: '/radiology-education', label: 'Radiology Education' },
-  { href: '/subspecialties', label: 'Subspecialties' },
+  { href: '/clinical-news', label: 'Clinical News', children: clinicalNews },
+  { href: '/imaging-informatics', label: 'Imaging Informatics', children: imagingInformatics },
+  { href: '/industry-news', label: 'Industry News', children: industryNews },
+  { href: '/practice-management', label: 'Practice Management', children: practiceManagement },
+  { href: '/radiology-education', label: 'Radiology Education', children: radiologyEducation },
+  { href: '/subspecialties', label: 'Subspecialties', children: subspecialties },
   ...(icleCfg.enabled ? [
     { href: `https://${icleCfg.hostname}/cases?VerifyLogin=1`, label: 'Cases', when: 'logged-in' },
     { href: `https://${icleCfg.hostname}/jobs?VerifyLogin=1`, label: 'Jobs', when: 'logged-in' },
     { href: `https://${icleCfg.hostname}/cases?VerifyLogin=0`, label: 'Cases', when: 'logged-out' },
     { href: `https://${icleCfg.hostname}/jobs?VerifyLogin=0`, label: 'Jobs', when: 'logged-out' },
+    { href: `https://${icleCfg.hostname}/forums?VerifyLogin=1`, label: 'Forums', when: 'logged-in' },
+    { href: `https://${icleCfg.hostname}/cme?VerifyLogin=1`, label: 'CME', when: 'logged-in' },
+    { href: `https://${icleCfg.hostname}/forums?VerifyLogin=0`, label: 'Forums', when: 'logged-out' },
+    { href: `https://${icleCfg.hostname}/cme?VerifyLogin=0`, label: 'CME', when: 'logged-out' },
   ] : []),
 ];
 
-const secondary = [
+const resources = [
   { href: '/about-us', label: 'About Us' },
-  { href: '/forums', label: 'Forums' },
   { href: '/advertising', label: 'Advertising' },
   { href: '/help', label: 'Help' },
   { href: '/resources/conference', label: 'Conferences' },
@@ -31,13 +86,8 @@ const secondary = [
 ];
 
 const mobileMenu = {
-  primary: sortNavItems([
-    ...topics,
-  ]),
-  secondary: sortNavItems([
-    ...secondary,
-    subscribe,
-  ]),
+  primary: topics,
+  secondary: resources,
 };
 
 module.exports = {
@@ -88,7 +138,7 @@ module.exports = {
   mobileMenu,
   topics,
   primary: {
-    items: secondary,
+    items: resources,
   },
   secondary: {
     items: topics,
@@ -96,67 +146,89 @@ module.exports = {
   tertiary: {
     items: [],
   },
+  contexts: [
+    {
+      when: ['/clinical-news'],
+      secondary: { items: topics },
+      tertiary: { items: [] },
+      primary: { items: clinicalNews },
+    },
+    {
+      when: ['/imaging-informatics'],
+      secondary: { items: topics },
+      tertiary: { items: [] },
+      primary: { items: imagingInformatics },
+    },
+    {
+      when: ['/industry-news'],
+      secondary: { items: topics },
+      tertiary: { items: [] },
+      primary: { items: industryNews },
+    },
+    {
+      when: ['/practice-management'],
+      secondary: { items: topics },
+      tertiary: { items: [] },
+      primary: { items: practiceManagement },
+    },
+    {
+      when: ['/radiology-education'],
+      secondary: { items: topics },
+      tertiary: { items: [] },
+      primary: { items: radiologyEducation },
+    },
+    {
+      when: ['/subspecialties'],
+      secondary: { items: topics },
+      tertiary: { items: [] },
+      primary: { items: subspecialties },
+    },
+  ],
   toggleMenu: {
     col1: {
-      label: 'Topic 1',
-      items: [
-        { href: '#', label: 'Subtopic 1' },
-        // ...sortNavItems(businessInsights),
-        // { href: '/business-insites', label: 'View all &raquo;' },
-      ],
+      label: 'Clinical News',
+      items: clinicalNews,
     },
     col2: {
-      label: 'Topic 2',
-      items: [
-        { href: '#', label: 'Subtopic 1' },
-        // ...sortNavItems(diagnosticTechnologies).slice(0, 10),
-        // { href: '/diagnostic-technologies', label: 'View all &raquo;' },
-      ],
+      label: 'Imaging Informatics',
+      items: imagingInformatics,
     },
     col3: {
-      label: 'Topic 3',
-      items: [
-        { href: '#', label: 'Subtopic 1' },
-        // ...sortNavItems(diseases).slice(0, 10),
-        // { href: '/diseases', label: 'View all &raquo;' },
-      ],
+      label: 'Industry News',
+      items: industryNews,
     },
     col4: {
-      label: 'Topic 4',
-      items: [
-        { href: '#', label: 'Subtopic 1' },
-        // ...resources,
-        // ...pointOfCareTesting,
-      ],
+      label: 'Practice Management',
+      items: practiceManagement,
+    },
+    col5: {
+      label: 'Resources',
+      items: resources,
     },
   },
   footer: {
     col1: {
       label: 'Topics',
-      items: topics,
+      colspan: 5,
+      items: [
+        { href: '/clinical-news', label: 'Clinical News' },
+        { href: '/imaging-informatics', label: 'Imaging Informatics' },
+        { href: '/industry-news', label: 'Industry News' },
+        { href: '/practice-management', label: 'Practice Management' },
+        { href: '/radiology-education', label: 'Radiology Education' },
+        { href: '/subspecialties', label: 'Subspecialties' },
+      ],
     },
     col2: {
       label: 'Resources',
-      items: [
-        { href: '#', label: 'Resource 1' },
-        // ...resources,
-        // { href: '#', label: 'TBD' },
-        // { href: '#', label: 'TBD' },
-      ],
-    },
-    col3: {
-      label: 'More',
-      items: [
-        { href: '#', label: 'More Item 1' },
-        // { href: '#', label: 'TBD' },
-        // { href: '#', label: 'TBD' },
-      ],
+      items: resources,
     },
     items: [
       privacyPolicy,
-      { href: '/page/copyright-information', label: 'Copyright Information' },
+      { href: '/page/terms-conditions', label: 'Terms & Conditions' },
       { href: '/page/contact-us', label: 'Contact Us' },
       { href: '/site-map', label: 'Site Map' },
+      { href: '/', label: 'Home' },
     ],
   },
 };
