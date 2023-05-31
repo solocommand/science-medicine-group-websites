0e36627290aed3f3e88ea26cb43a917e1df6d47a
Josh Worden <josh@parameter1.com>
2023-05-31 14:06:57 -0500

    Pass req/res to contentGating handler

--- packages/global/middleware/content-gating.js
+++ packages/global/middleware/content-gating.js
@@ -13,29 +13,32 @@ const COOKIE_NAME = '__idx_gating';
  *
  * @returns Promise([Boolean, String[]])
  */
-module.exports = (app) => contentGating(app, true, ({ content, req, res }) => {
-  const { identityX } = req;
-  const requiresRegistration = get(content, 'userRegistration.isCurrentlyRequired');
-  const accessLevels = getAsArray(content, 'userRegistration.accessLevels');
+module.exports = (app) => app.use((req, res, next) => {
+  contentGating(app, true, ({ content }) => {
+    const { identityX } = req;
+    const requiresRegistration = get(content, 'userRegistration.isCurrentlyRequired');
+    const accessLevels = getAsArray(content, 'userRegistration.accessLevels');
 
-  // If the content isn't gated, do nothing.
-  if (!requiresRegistration) return [requiresRegistration, accessLevels];
+    // If the content isn't gated, do nothing.
+    if (!requiresRegistration) return [requiresRegistration, accessLevels];
 
-  // If a user is logged in, enable and don't do anything special.
-  if (identityX && identityX.token) return [requiresRegistration, accessLevels];
+    // If a user is logged in, enable and don't do anything special.
+    if (identityX && identityX.token) return [requiresRegistration, accessLevels];
 
-  // Check for `__idx_gating` cookie, set by Braze Identity middleware
-  const cookie = getCookie({ req, res, name: COOKIE_NAME });
+    // Check for `__idx_gating` cookie, set by Braze Identity middleware
+    const cookie = getCookie({ req, res, name: COOKIE_NAME });
 
-  if (cookie) {
-    try {
-      const allowed = JSON.parse(cookie);
-      return [!allowed, accessLevels];
-    } catch (e) {
-      // noop
+    if (cookie) {
+      try {
+        const allowed = JSON.parse(cookie);
+        return [!allowed, accessLevels];
+      } catch (e) {
+        // noop
+      }
     }
-  }
 
-  // Final fallback
-  return [requiresRegistration, accessLevels];
+    // Final fallback
+    return [requiresRegistration, accessLevels];
+  });
+  next();
 });
