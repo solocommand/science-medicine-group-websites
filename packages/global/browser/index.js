e61b2291651947f922f57dc2c36c46c656891671
Jake Collins <shinsina@att.net>
2023-07-19 11:53:12 -0500

    Create and register collapsable-sidebar component

--- packages/global/browser/index.js
+++ packages/global/browser/index.js
@@ -4,6 +4,7 @@ import Braze from '@science-medicine-group/package-braze/browser';
 import FormLogin from './form-login.vue';
 import Rudderstack from './rudderstack.vue';
 import Auth0Authenticated from './auth0-authenticated.vue';
+import CollapsableSidebar from './collapsable-sidebar.vue';
 
 const GlobalNewsletterMenu = () => import(/* webpackChunkName: "global-newsletter-menu" */ './newsletter-menu.vue');
 const ContentMeterTrack = () => import(/* webpackChunkName: "content-meter-tracker" */ './track-content-meter.vue');
@@ -26,6 +27,7 @@ export default (Browser) => {
   });
   Auth0(Browser);
   Braze(Browser);
+  Browser.register('CollapsableSidebar', CollapsableSidebar);
 
   // Rudderstack identification
   Browser.register('Rudderstack', Rudderstack, { provide: { EventBus } });
