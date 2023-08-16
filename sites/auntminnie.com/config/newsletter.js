df9af61c186fc3ab39fee9a236b71dfccb3faed4
Josh Worden <josh@parameter1.com>
2023-08-16 15:32:01 -0500

    Update newsletter configs for AM

--- sites/auntminnie.com/config/newsletter.js
+++ sites/auntminnie.com/config/newsletter.js
@@ -1,20 +1,21 @@
 const defaults = {
   name: 'Stay in the Know',
-  description: 'Delivered right to your inbox, Aunt Minnies’s newsletters. Subscribe to get exclusive access!',
+  description: 'Delivered right to your inbox, Aunt Minnie\'s newsletters. Subscribe to get exclusive access!',
 };
 
 module.exports = {
-  pushdown: {
-    ...defaults,
-    imagePath: 'files/base/smg/all/image/static/',
-  },
   signupBanner: {
     ...defaults,
+    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-full.png',
+  },
+  pushdown: {
+    ...defaults,
+    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-half.png',
   },
   signupFooter: {
     ...defaults,
+    name: 'Stay Connected',
     colspan: 3,
     enable: true,
-    name: 'Stay Connected',
   },
 };
--- sites/auntminnieasia.com/config/newsletter.js
+++ sites/auntminnieasia.com/config/newsletter.js
@@ -1,71 +1,19 @@
-const privacyPolicy = require('./privacy-policy');
-
-const baseConfig = {
-  action: 'https://sciencemedicinegroup.dragonforms.com/loading.do',
-  hiddenInputs: [
-    { name: 'omedasite', value: 'sab_subscriptions' },
-  ],
-};
-
 const defaults = {
-  name: 'Don’t Miss Out',
-  description: 'Get the business tips, industry insights and trending news every professional needs to know.',
-  defaultNewsletter: {
-    deploymentTypeId: 29,
-    name: 'Daily Report',
-    eventCategory: 'Daily Newsletter Subscription',
-  },
-  privacyPolicy,
-  newsletters: [
-    {
-      deploymentTypeId: 30,
-      name: 'Equipment Weekly',
-      description: 'Roundup of news and reviews',
-      eventCategory: 'Weekly Equipment Subscription',
-    },
-    {
-      deploymentTypeId: 31,
-      name: 'Technology Weekly',
-      description: 'Top tech developments in the industry',
-      eventCategory: 'Weekly Technology Subscription',
-    },
-    {
-      deploymentTypeId: 32,
-      name: 'Weekend Newsletter',
-      description: 'The top news of the week in the industry',
-      eventCategory: 'Weekend Newsletter',
-    },
-  ],
-  demographic: {
-    id: 72,
-    label: 'Your primary role?',
-    values: [
-      { id: 123, label: 'Other' },
-    ],
-  },
+  name: 'Stay in the Know',
+  description: 'Delivered right to your inbox, Aunt Minnie\'s newsletters. Subscribe to get exclusive access!',
 };
 
 module.exports = {
-  // uses inline omeda form
   signupBanner: {
     ...defaults,
-    imagePath: 'files/base/smg/all/image/static/newsletter-pushdown/sab-full.png',
+    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-full.png',
   },
   pushdown: {
     ...defaults,
-    imagePath: 'files/base/smg/all/image/static/newsletter-pushdown/sab-half.png',
-    description: 'Description',
-  },
-
-  // links off to seperate omeda dragonform
-  signupBannerLarge: {
-    ...baseConfig,
-    name: 'Don’t Miss Out',
-    description: 'Description',
+    imagePath: 'files/base/p1/sandbox/image/static/newsletter-phone-half.png',
   },
   signupFooter: {
-    ...baseConfig,
-    name: 'Newsletter for Professionals',
-    description: 'Description',
+    ...defaults,
+    name: 'Stay Connected',
   },
 };
