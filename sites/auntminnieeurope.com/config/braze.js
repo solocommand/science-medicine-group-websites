0d7ac39cf8f890679ac7e12138e194b9545746f7
Josh Worden <josh@parameter1.com>
2023-08-22 10:47:08 -0500

    Fix braze site name

--- sites/auntminnieeurope.com/config/braze.js
+++ sites/auntminnieeurope.com/config/braze.js
@@ -1,7 +1,7 @@
 const config = require('@science-medicine-group/package-global/config/braze');
 
 module.exports = config({
-  siteName: 'AME',
+  siteName: 'AuntMinnieEurope',
   defaultGroupId: '6bbda052-75b3-4c48-bbbe-a943a1b7beac',
   unconfirmedGroupId: '38680d9c-1fc4-4b44-b115-be21d2203a6b',
   appGroupId: '634d675d1118243d1113480e',
