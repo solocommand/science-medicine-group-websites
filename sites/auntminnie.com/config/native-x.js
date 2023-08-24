bd682829223f24f88dcb7a5a31b3d4375822951d
Jessica Wade <jessica.wade@parameter1.com>
2023-08-24 12:54:37 -0500

    Add ‘Cases’ native-ad slot

--- sites/auntminnie.com/config/native-x.js
+++ sites/auntminnie.com/config/native-x.js
@@ -8,6 +8,9 @@ config
   .setAliasPlacements('default', [
     { name: 'default', id: '62fce200ffb1230001dca815' },
     { name: 'related-content', id: '62fce200ffb1230001dca815' },
+  ])
+  .setAliasPlacements('cases', [
+    { name: 'cases', id: '64e7906396af9a00010dab91' },
   ]);
 
 module.exports = config;
