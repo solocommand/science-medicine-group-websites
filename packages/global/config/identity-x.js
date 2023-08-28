a76f14205fc04c08aba0e3c71d7d11e7db8c7978
Josh Worden <josh@parameter1.com>
2023-08-28 13:02:43 -0500

    Remove ZeroBounce validation from IdentityX flow

--- packages/global/config/identity-x.js
+++ packages/global/config/identity-x.js
@@ -1,5 +1,4 @@
 const IdentityXConfiguration = require('@parameter1/base-cms-marko-web-identity-x/config');
-const emailValidator = require('@science-medicine-group/package-zero-bounce/validator');
 const newrelic = require('newrelic');
 
 module.exports = ({
@@ -31,7 +30,6 @@ module.exports = ({
     requiredClientFields,
     booleanQuestionsLabel: '',
     onHookError: newrelic.noticeError.bind(newrelic),
-    emailValidator,
     defaultFieldLabels,
     enableChangeEmail: true,
     ...rest,
