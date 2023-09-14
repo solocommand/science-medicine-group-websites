1d6efd15b9b8e27f861750f4737414cb6d04dcdf
Jessica Wade <jessica.wade@parameter1.com>
2023-09-14 09:15:35 -0500

    Add Buyer’s Guide

--- sites/auntminnie.com/config/navigation.js
+++ sites/auntminnie.com/config/navigation.js
@@ -77,6 +77,7 @@ const resources = [
   { href: '/resources/videos', label: 'Videos' },
   { href: '/resources/webinars', label: 'Webinars' },
   { href: '/resources/vendors', label: 'Vendors' },
+  { href: 'https://issuu.com/auntminnie/docs/230206_2023ambuyersguide_nobleed', label: 'Buyer\'s Guide' },
 ];
 
 const more = [
