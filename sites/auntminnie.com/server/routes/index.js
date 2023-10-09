afacff3e5c76127546e1014bbd1b5ff85c421fb9
Shinsina <shinsina@att.net>
2023-10-09 15:06:56 -0500

    Additional blanket redirects for pathes/params, AM

--- sites/auntminnie.com/server/routes/index.js
+++ sites/auntminnie.com/server/routes/index.js
@@ -33,12 +33,37 @@ module.exports = (app) => {
   app.use(asyncRoute(async (req, res, next) => {
     const { query: reqQuery } = req;
     if (!reqQuery) return next();
+    if (getParameterCaseInsensitive(reqQuery, 'sec') === 'edu') {
+      res.redirect(301, 'https://my.auntminnie.com/cases');
+    }
+    if (getParameterCaseInsensitive(reqQuery, 'sec') === 'log'
+    || getParameterCaseInsensitive(reqQuery, 'sec') === 'def'
+    || getParameterCaseInsensitive(reqQuery, 'sec') === 'eba'
+    || getParameterCaseInsensitive(reqQuery, 'sec') === 'cas'
+    || getParameterCaseInsensitive(reqQuery, 'sec') === 'dsc') {
+      res.redirect(301, '/');
+    }
+    if (getParameterCaseInsensitive(reqQuery, 'sub') === 'bsp'
+      || getParameterCaseInsensitive(reqQuery, 'sub') === 'prm') {
+      res.redirect(301, '/page/advertising');
+    }
+    if (getParameterCaseInsensitive(reqQuery, 'sub') === 'br') {
+      res.redirect(301, '/page/area-not-found');
+    }
+    if (getParameterCaseInsensitive(reqQuery, 'sec') === 'olce') {
+      res.redirect(301, 'https://my.auntminnie.com/cases/');
+    }
+    if (getParameterCaseInsensitive(reqQuery, 'sec') === 'cns') {
+      res.redirect(301, '/resources/conference');
+    }
     if (getParameterCaseInsensitive(reqQuery, 'sec') === 'cls'
-      || getParameterCaseInsensitive(reqQuery, 'sub') === 'emp'
-    ) res.redirect(301, 'https://auntminnie.careerwebsite.com/');
+      || getParameterCaseInsensitive(reqQuery, 'sub') === 'emp') {
+      res.redirect(301, 'https://auntminnie.careerwebsite.com/');
+    }
     if (getParameterCaseInsensitive(reqQuery, 'sec') === 'olc'
-      || getParameterCaseInsensitive(reqQuery, 'sub') === 'cotx'
-    ) res.redirect(301, '/page/case-not-found');
+      || getParameterCaseInsensitive(reqQuery, 'sub') === 'cotx') {
+      res.redirect(301, '/page/case-not-found');
+    }
     return next();
   }));
 
@@ -46,6 +71,26 @@ module.exports = (app) => {
   app.use(asyncRoute(async (req, res, next) => {
     if (req.path.match(/^\/radiology-jobs/)) {
       res.redirect(301, 'https://auntminnie.careerwebsite.com/');
+    }
+    // Any url whose path starts with /rss to /feed
+    if (req.path.match(/^\/rss/)) {
+      res.redirect(301, '/feed');
+    }
+    // Any url whose path starts with /Digital-X-Ray to /clinical-news/digital-x-ray
+    if (req.path.match(/^\/Digital-X-Ray/)) {
+      res.redirect(301, '/clinical-news/digital-x-ray');
+    }
+    // Any url whose path starts with /Molecular-Imaging to /clinical-news/molecular-imaging
+    if (req.path.match(/^\/Molecular-Imaging/)) {
+      res.redirect(301, '/clinical-news/molecular-imaging');
+    }
+    // Any url whose path starts with /Ultrasound to /clinical-news/ultrasound
+    if (req.path.match(/^\/Ultrasound/)) {
+      res.redirect(301, '/clinical-news/ultrasound');
+    }
+    // Path starts with /Artificial-Intelligence to /imaging-informatics/artificial-intelligence
+    if (req.path.match(/^\/Artificial-Intelligence/)) {
+      res.redirect(301, '/imaging-informatics/artificial-intelligence');
     }
     return next();
   }));
