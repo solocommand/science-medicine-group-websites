896a5e98a50e596af4ff20de77e858513e8bf6e6
Shinsina <shinsina@att.net>
2024-01-24 11:43:58 -0600

    Incorporate reading cookies into modal firing

--- packages/global/browser/newsletter-signup-modal-listener.vue
+++ packages/global/browser/newsletter-signup-modal-listener.vue
@@ -3,13 +3,38 @@
 </template>
 
 <script>
+import cookies from 'js-cookie';
+
 export default {
+  props: {
+    cookieName: {
+      type: String,
+      required: true,
+    },
+    cookieValue: {
+      type: String,
+      default: '0',
+    },
+    hasCookie: {
+      type: Boolean,
+      required: true,
+    },
+  },
   created() {
     setTimeout(() => {
+      const newsletterSignupModalElement = document.getElementById('newsletter-signup-modal');
       // @todo Correct newsletterSignupType to a specific type for this signup position
-      if (window.dataLayer.find((dataEvent) => dataEvent['identity-x'] && dataEvent['identity-x'].newsletterSignupType === 'inlineContent')) {
-        const newsletterSignupModalElement = document.getElementById('newsletter-signup-modal');
+      if (
+        this.hasCookie
+        && this.cookieValue === '0'
+        && window.dataLayer.find((dataEvent) => dataEvent['identity-x']
+        && dataEvent['identity-x'].newsletterSignupType === 'inlineContent')
+      ) {
         newsletterSignupModalElement.classList.add('newsletter-signup-modal-fade-in');
+        // Set cookie, expires is in days so we have to divide by 24 for hours
+        cookies.set(this.cookieName, '1', { expires: 1 / 24 });
+      } else if (!this.hasCookie) {
+        cookies.set(this.cookieName, '0', { expires: 1 });
       }
     }, 5000);
   },
