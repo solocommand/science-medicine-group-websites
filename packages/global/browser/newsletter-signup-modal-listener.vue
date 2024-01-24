ec029a99190388f16d8082ee2d94860397186f0c
Shinsina <shinsina@att.net>
2024-01-24 10:32:46 -0600

    Create and register newsletter-signup-modal-listener

---
+++ packages/global/browser/newsletter-signup-modal-listener.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="display: none;" />
+</template>
+
+<script>
+export default {
+  created() {
+    setTimeout(() => {
+      // @todo Correct newsletterSignupType to a specific type for this signup position
+      if (window.dataLayer.find((dataEvent) => dataEvent['identity-x'] && dataEvent['identity-x'].newsletterSignupType === 'inlineContent')) {
+        const newsletterSignupModalElement = document.getElementById('newsletter-signup-modal');
+        newsletterSignupModalElement.classList.add('newsletter-signup-modal-fade-in');
+      }
+    }, 5000);
+  },
+};
+</script>
