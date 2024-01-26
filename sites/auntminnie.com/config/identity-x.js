69fd7e222bdfd4282e98a4ad5126c1c1869350df
Brian Miller <brian@parameter1.com>
2024-01-26 15:28:26 -0600

    Add default form for idx & specifically to AM

--- sites/auntminnie.com/config/identity-x.js
+++ sites/auntminnie.com/config/identity-x.js
@@ -1,11 +1,37 @@
 const configureIdentityX = require('@science-medicine-group/package-global/config/identity-x');
+const formDefault = require('@science-medicine-group/package-global/config/identity-x/default');
+
+formDefault.anonymousCta = 'Register on AuntMinnie.com to download this document, gain access to premium content, and more.';
+
+// append custom demos row of questions to the end of general questions
+formDefault.fieldRows.push(
+  [
+    {
+      label: 'Profession',
+      id: '642eec7ee5f1a5e91d60f946',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+    {
+      label: 'Function',
+      id: '642ee9eb3e8177b0b6dc7bca',
+      type: 'custom-select',
+      required: true,
+      width: 0.5,
+    },
+  ],
+);
 
 module.exports = configureIdentityX({
   appId: process.env.IDENTITYX_APPID || '62a20ac739347c1810862985',
   activeCustomFieldIds: [
-    '642ee9eb3e8177b0b6dc7bca', // Title
+    '642ee9eb3e8177b0b6dc7bca', // Profession
     '642eec7ee5f1a5e91d60f946', // Function
   ],
+  forms: {
+    default: formDefault,
+  },
   gtmUserFields: {
     function: '642eec7ee5f1a5e91d60f946',
     profession: '642ee9eb3e8177b0b6dc7bca',
