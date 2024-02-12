84de6d3989b1cd6767ccd5e9223f5a90b82fcf01
Shinsina <shinsina@att.net>
2024-02-12 09:58:14 -0600

    Auto-signup verified users on-login-link-sent

--- packages/braze/hooks/on-login-link-sent.js
+++ packages/braze/hooks/on-login-link-sent.js
@@ -1,14 +1,29 @@
 /**
+ * @typedef {import('@parameter1/base-cms-marko-web-identity-x/service')} IdentityXService
+ * @typedef {import('../service')} BrazeService
+ * @typedef {import('../index').BrazeConfig} BrazeConfig
  * @typedef {import("../service")} BrazeService
  *
  * @typedef IdentityXRequest
  * @prop {BrazeService} braze
+ *
+ * @typedef {import('./on-authentication-success').IdentityXHookArgs} IdentityXHookArgs
+ * @param {IdentityXHookArgs} args
  */
-module.exports = async ({ user, service }) => {
+module.exports = async ({
+  additionalEventData,
+  brazeConfig,
+  user,
+  service,
+}) => {
   /** @type {IdentityXRequest} */
   const { braze } = service.req;
 
   if (user && !user.verified) await braze.unconfirmUser(user.email, user.id);
+
+  if (user && user.verified && additionalEventData.actionSource === 'newsletterSignup') {
+    await braze.updateSubscriptions(user.email, user.id, { [brazeConfig.defaultGroupId]: true });
+  }
 
   return user;
 };
