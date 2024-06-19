b03711909370388f1ef61cde730c1fb841df8d6d
Shinsina <shinsina@att.net>
2024-06-19 12:44:23 -0500

    Create and register BrazeP1EventsIdentify Vue component

---
+++ packages/braze/browser/p1-events-identify.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p1-events-identity-x" style="display: none;" />
+</template>
+
+<script>
+
+export default {
+  props: {
+    identifier: {
+      type: String,
+      required: true,
+    },
+  },
+  created() {
+    window.p1events('setIdentity', this.identifier);
+  },
+};
+</script>
