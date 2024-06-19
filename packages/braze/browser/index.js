3fffe7b0d8fe6887127e3a905a93138ef55410c0
Shinsina <shinsina@att.net>
2024-06-19 14:08:03 -0500

    Remove BrazeP1EventsIdentify component def and registration

--- packages/braze/browser/index.js
+++ packages/braze/browser/index.js
@@ -1,7 +1,5 @@
 const PreferenceCenter = () => import(/* webpackChunkName: "braze-preference-center" */ './preference-center.vue');
-const P1EventsIdentify = () => import(/* webpackChunkName: "braze-p1-events-identify" */ './p1-events-identify.vue');
 
 export default (Browser) => {
   Browser.register('BrazePreferenceCenter', PreferenceCenter);
-  Browser.register('BrazeP1EventsIdentify', P1EventsIdentify);
 };
