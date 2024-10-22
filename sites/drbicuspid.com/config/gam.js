6abc624aca89a351fd98ff98fab3253bd9c21ef3
Jessica Wade <jessica.wade@parameter1.com>
2024-10-22 09:34:32 -0500

    Update conference ad urls

--- sites/drbicuspid.com/config/gam.js
+++ sites/drbicuspid.com/config/gam.js
@@ -45,8 +45,8 @@ const aliases = [
   { alias: 'resources/webinars' },
   { alias: 'resources/vendors' },
   { alias: 'demo' },
-  { alias: 'resources/conferences/ids' },
-  { alias: 'resources/conferences/cds-midwinter' },
+  { alias: 'resources/conferences/ids-2025' },
+  { alias: 'resources/conferences/cds-midwinter-2025' },
 ];
 
 aliases.forEach(({ alias, prefix = alias }) => config.setAliasAdUnits(alias, [
